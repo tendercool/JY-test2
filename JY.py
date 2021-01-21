9417854eee332d8f49432f0a46881029832e9390
tendercool <93910468@qq.com>
2021-01-21 14:40:39 +0800

    tendercool

--- JY.py
+++ JY.py
@@ -1,5 +1,8 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow,QMessageBox
 from PyQt5.Qt import QApplication,qApp
+from PyQt5.QtGui import QPixmap
+import serial
+import serial.tools.list_ports
 import port_connection
 import sys
 import Ui_JY1_newset
@@ -17,21 +20,59 @@ class JY_Main(QMainWindow,Ui_JY1_newset.Ui_MainWindow):
         self.port_connect = port_connection.port_connect()
         self.tabWidget.setEnabled(False)
         self.btn_checkport.clicked.connect(self.port_check)
+        self.btn_connect_port.clicked.connect(self.port_connection)
+        self.btn_disconnect_port.clicked.connect(self.port_close)
        
 
     def port_check(self):
-        self.baud = self.comboBox_baud.currentText()
+        self.comboBox_port.clear()
+        com = serial.Serial()
+        coms_s = {}
+        com_list = serial.tools.list_ports.comports()
+        
+        if len(com_list) <= 0 :
+            QMessageBox.critical(self,'错误','无可用端口')
+        else:
+            for coms in com_list:
+                coms_s['%s'%coms[0]] = '%s'%coms[1]
+                self.comboBox_port.addItem(coms[0])
+        com.close()
+            
+      
+
+    def port_connection(self):
+        self.baud = int(self.comboBox_baud.currentText())
         self.parity = self.comboBox_parity.currentText()
-        self.data = self.comboBox_byte.currentText()
-        self.stop = self.comboBox_stop.currentText()
-        self.port = self.port_connect.check()
-        self.comboBox_port.addItem(self.port)
+        if self.parity == '无':
+            self.parity = 'N'
+        if self.parity == '奇校验':
+            self.parity = 'O'
+        if self.parity == '偶校验':
+            self.parity = 'E'
+        self.data = int(self.comboBox_byte.currentText())
+        self.stop = int(self.comboBox_stop.currentText())
+        self.port = self.comboBox_port.currentText()
+        self.port_connect.connect(self.port,self.baud,self.parity,self.data,self.stop)
+        if self.port_connect.ser.isOpen():
+            QMessageBox.warning(self,'提示','%s串口已打开'%self.port)
+            self.tabWidget.setEnabled(True)
+            self.btn_checkport.setEnabled(False)
+            self.btn_connect_port.setEnabled(False)
+            self.groupBox_2.setEnabled(False)
+            self.port_signal.setPixmap(QPixmap('g:\\PyQt\\JY-test2\\image/green_pic.png'))
+            self.manger_signal.setPixmap(QPixmap('g:\\PyQt\\JY-test2\\image/green_pic.png'))
+            
 
-    def port_connect(self):
 
-        self.tabWidget.setEnabled(True)
-        
-        
+    def port_close(self):
+        self.port_connect.disconnect()
+        if self.port_connect.ser.isOpen() == 0:
+            self.tabWidget.setEnabled(False)
+            self.btn_checkport.setEnabled(True)
+            self.btn_connect_port.setEnabled(True)
+            self.groupBox_2.setEnabled(True)
+            self.port_signal.setPixmap(QPixmap('g:\\PyQt\\JY-test2\\image/red_pic.png'))
+            self.manger_signal.setPixmap(QPixmap('g:\\PyQt\\JY-test2\\image/red_pic.png'))
 
 
 if __name__ == '__main__':
--- port_connection.py
+++ port_connection.py
@@ -1,36 +1,37 @@
 from PyQt5.Qt import QThread,QObject
-from PyQt5.QtSerialPort import QSerialPort,QSerialPortInfo
+import serial
+import serial.tools.list_ports
+from PyQt5.QtWidgets import QMessageBox,QWidget
 
 
-class port_connect(QObject):
+class port_connect(QWidget):
     def __init__(self):
         super().__init__()
         self.port_state = 0
-        self.ser = QSerialPort()
-        self.ser_info = QSerialPortInfo()
-
-        self.check()
-
+        self.ser = serial.Serial()
 
     def set_port(self):
-        if self.check_signal != '':
-            self.check()
+        
         self.connect()
         self.disconnect()
         self.send()
         self.read()
 
-    def check(self):
-        port_info = self.ser_info.availablePorts()
-        for port in port_info:
-            port_id = port.portName()
-        return port_id
+    
 
-    def connect(self):
-        pass
+    def connect(self,port,baud,parity,data,stop):
+        self.ser.port = port
+        self.ser.baudrate = baud
+        self.ser.parity = parity
+        self.ser.bytesize = data
+        self.ser.stopbit = stop
+        try:
+            self.ser.open()
+        except:
+            QMessageBox.critical(self,'错误','当前端口无法打开')
 
     def disconnect(self):
-        pass
+        self.ser.close()
 
     def read(self):
         pass
