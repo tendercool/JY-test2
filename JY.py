1df7498307cfedee8058b4effa338c732327589a
tendercool <93910468@qq.com>
2021-01-29 16:52:10 +0800

    tendercool

--- JY.py
+++ JY.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QMainWindow,QMessageBox
-from PyQt5.Qt import QApplication,qApp
+from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from PyQt5.Qt import QApplication, qApp
 from PyQt5.QtGui import QPixmap
 import serial
 import serial.tools.list_ports
@@ -7,12 +7,10 @@ import port_connection
 import sys
 import Ui_JY1_newset
 import tcp_connection
-from PyQt5.QtCore import QThread,pyqtSignal,QTimer
+from PyQt5.QtCore import QThread, pyqtSignal, QTimer
 
 
-
-
-class JY_Main(QMainWindow,Ui_JY1_newset.Ui_MainWindow):
+class JY_Main(QMainWindow, Ui_JY1_newset.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -20,35 +18,33 @@ class JY_Main(QMainWindow,Ui_JY1_newset.Ui_MainWindow):
         with open("style.qss", 'r') as f:
             qApp.setStyleSheet(f.read())
 
-        # self.port_connect = port_connection.port_connect()
         self.tabWidget.setEnabled(False)
         self.btn_checkport.clicked.connect(self.port_check)
         self.btn_connect_port.clicked.connect(self.port_connection)
         self.btn_disconnect_port.clicked.connect(self.port_close)
         self.btn_city_on.clicked.connect(self.btn_city_on_cb)
         self.btn_city_off.clicked.connect(self.btn_city_off_cb)
-       
+
         self.ctiy_flag = 0
         self.val_str = ''
         self.time_refresh = QTimer()
         self.time_refresh.start(1000)
         self.time_refresh.timeout.connect(self.val_update)
 
+        self.btn_disconnect_port.setEnabled(False)
+
     def port_check(self):
         self.comboBox_port.clear()
         com = serial.Serial()
         coms_s = {}
         com_list = serial.tools.list_ports.comports()
-        
-        if len(com_list) <= 0 :
-            QMessageBox.critical(self,'错误','无可用端口')
+        if len(com_list) <= 0:
+            QMessageBox.critical(self, '错误', '无可用端口')
         else:
             for coms in com_list:
-                coms_s['%s'%coms[0]] = '%s'%coms[1]
+                coms_s['%s' % coms[0]] = '%s' % coms[1]
                 self.comboBox_port.addItem(coms[0])
         com.close()
-            
-    
 
     def port_connection(self):
         self.baud = int(self.comboBox_baud.currentText())
@@ -62,62 +58,44 @@ class JY_Main(QMainWindow,Ui_JY1_newset.Ui_MainWindow):
         self.data = int(self.comboBox_byte.currentText())
         self.stop = int(self.comboBox_stop.currentText())
         self.port = self.comboBox_port.currentText()
-
-        port_info = [self.port,self.baud,self.parity,self.data,self.stop]
+        port_info = [self.port, self.baud, self.parity, self.data, self.stop]
         self.port_connect = port_connection.port_connect(port_info)
-        # self.port_connect.connect(self.port,self.baud,self.parity,self.data,self.stop)
-        # self.port_thread = QThread()
-        # self.port_connect = 
         self.port_connect.oksignal.connect(self.checkok)
         self.port_connect.wrongsignal.connect(self.checkno)
-        # self.port_connect.moveToThread(self.port_thread)
-        # self.port_thread.started.connect(self.port_connect.connect)
         self.port_connect.start()
-        
-        # self.port_connect.send_msg.moveToThread(self.port_connect)
-
         self.port_connect.read_msg_signal.connect(self.datarec)
+        self.btn_disconnect_port.setEnabled(True)
 
-        
-    
-    def datarec(self,data):
+    def datarec(self, data):
         self.val_str = data
-        # return val_str
 
-    def checkno(self,str):
-        QMessageBox.warning(self,'Wrong',str)
+    def checkno(self, str):
+        QMessageBox.warning(self, 'Wrong', str)
         self.port_connect.terminate()
-    
-    def checkok(self,str):
-        QMessageBox.warning(self,'提示','%s串口已打开'%self.port)
+
+    def checkok(self, str):
+        QMessageBox.warning(self, '提示', '%s串口已打开' % self.port)
         self.tabWidget.setEnabled(True)
         self.btn_checkport.setEnabled(False)
         self.btn_connect_port.setEnabled(False)
         self.groupBox_2.setEnabled(False)
         self.port_signal.setPixmap(QPixmap('./image/green_pic.png'))
-        self.manger_signal.setPixmap(QPixmap('./image/green_pic.png'))          
-
+        self.manger_signal.setPixmap(QPixmap('./image/green_pic.png'))
 
     def port_close(self):
-        # self.port_connect.disconnect()
         self.port_connect.port_state = 0
-        # if self.port_connect.connect.ser.isOpen() == 0:
         self.tabWidget.setEnabled(False)
         self.btn_checkport.setEnabled(True)
         self.btn_connect_port.setEnabled(True)
         self.groupBox_2.setEnabled(True)
         self.port_signal.setPixmap(QPixmap('./image/red_pic.png'))
         self.manger_signal.setPixmap(QPixmap('./image/red_pic.png'))
+        self.btn_disconnect_port.setEnabled(False)
 
     def btn_city_on_cb(self):
         msg = 'AABBCCDDEEFF'
         self.port_connect.send_msg(msg)
         self.ctiy_flag = 1
-
-        # self.city_power_p_val.setText('5000')
-        # self.city_power_q_val.setText('3000')
-        # self.city_voltage_val.setText('300')
-        # self.city_current_val.setText('200')
 
     def btn_city_off_cb(self):
         self.ctiy_flag = 0
@@ -129,14 +107,11 @@ class JY_Main(QMainWindow,Ui_JY1_newset.Ui_MainWindow):
     def val_update(self):
         val_get = self.val_str
         if self.ctiy_flag == 1:
-            self.city_power_p_val.setText(str(int(val_get[0:4],16)) + 'W')
-            self.city_power_q_val.setText(str(int(val_get[5:8],16)) + 'W')
-            self.city_voltage_val.setText(str(int(val_get[9:12],16)) + 'V')
-            self.city_current_val.setText(str(int(val_get[12:16],16)) + 'A')
-            # self.city_power_p_val.setText(val_get[0:4])
-            # self.city_power_q_val.setText(val_get[4:8])
-            # self.city_voltage_val.setText(val_get[8:12])
-            # self.city_current_val.setText(val_get[12:16])
+            self.city_power_p_val.setText(str(int(val_get[0:4], 16)) + 'W')
+            self.city_power_q_val.setText(str(int(val_get[4:8], 16)) + 'W')
+            self.city_voltage_val.setText(str(int(val_get[8:12], 16)) + 'V')
+            self.city_current_val.setText(str(int(val_get[12:16], 16)) + 'A')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- port_connection.py
+++ port_connection.py
@@ -1,33 +1,21 @@
 # from PyQt5.Qt import QObject
 import serial
 import serial.tools.list_ports
-from PyQt5.QtWidgets import QMessageBox,QWidget
-from PyQt5.QtCore import QThread,pyqtSignal,QTimer
+from PyQt5.QtWidgets import QMessageBox, QWidget
+from PyQt5.QtCore import QThread, pyqtSignal, QTimer
 import time
+
 
 class port_connect(QThread):
     oksignal = pyqtSignal(str)
     wrongsignal = pyqtSignal(str)
     closesignal = pyqtSignal(str)
     read_msg_signal = pyqtSignal(str)
-    def __init__(self,list):
+
+    def __init__(self, list):
         super().__init__()
         self.port_state = 0
-        # self.ser = serial.Serial()
-        # self.ser.port = port
-        # self.ser.baudrate = baud
-        # self.ser.parity = parity
-        # self.ser.bytesize = data
-        # self.ser.stopbit = stop
         self.port_info = list
-        # self.ser.port = self.port_info[0]
-        # self.ser.baudrate = self.port_info[1]
-        # self.ser.parity = self.port_info[2]
-        # self.ser.bytesize = self.port_info[3]
-        # self.ser.stopbit = self.port_info[4]
-        # self.connect()
-    
-       
 
     def run(self):
         self.ser = serial.Serial()
@@ -40,50 +28,36 @@ class port_connect(QThread):
         self.serCloseFlag = 0
         try:
             self.ser.open()
-            # print(self.ser.port)
             self.port_state = 1
             self.oksignal.emit('OK')
-               
+
         except:
-            # QMessageBox.critical(self,'错误','当前端口无法打开')
             self.wrongsignal.emit('当前端口无法打开')
             self.port_state = 0
 
-    # def disconnect(self):
-    #     self.ser.close()  
         while self.port_state == 1:
             self.read()
             time.sleep(1)
-            # QThread.sleep(1)
-        # self.time_send = QTimer()
-        # self.time_send.start(1000)
-        # self.time_send.timeout.connect(self.read)
-            
-        
-   
 
     def read(self):
         try:
             num = self.ser.inWaiting()
             print(num)
-            
-        except :
+
+        except:
             self.ser.close()
             return None
         if num > 0:
             data = self.ser.read(num)
-            
             num = len(data)
             out_s = ''
-            for i in range(0,num):
+            for i in range(0, num):
                 out_s = out_s + '{:02X}'.format(data[i])
-                
             self.read_msg_signal.emit(out_s)
-            
         else:
             self.serCloseFlag = 1
 
-    def send_msg(self,msg):
+    def send_msg(self, msg):
         if self.ser.isOpen():
             msg_s = msg.strip()
             msg_s = bytes(msg_s.encode('utf-8'))
